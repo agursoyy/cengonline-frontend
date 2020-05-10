cf0e5d7328cb9d600a9ba5e3f50bdc064f4d971c
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-05-10 03:53:48 +0300

    daha general

--- src/pages/authentication/signup.tsx
+++ src/pages/authentication/signup.tsx
@@ -5,24 +5,17 @@ import { Link } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-
 const LoginSchema = Yup.object().shape({
-  name: Yup.string()
-    .required('Required'),
-  surname: Yup.string()
-    .required('Required'),
-  email: Yup.string()
-    .email('Invalid email')
-    .required('Required'),
-  password: Yup.string()
-    .required('Required')
-    .min(8, 'Too Short')
+  name: Yup.string().required('Required'),
+  surname: Yup.string().required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required('Required').min(6, 'Too Short'),
 });
 
 const Signup = () => {
   return (
     <div className="auth-container d-flex align-items-center">
-      < div className="container" >
+      <div className="container">
         <div className="row d-flex justify-content-center">
           <div className="col-md-5 p-0">
             <div className="auth-container__box">
@@ -51,52 +44,101 @@ const Signup = () => {
                 }}
               >
                 {({ errors, touched, values, isSubmitting }) => (
-
                   <Form noValidate>
                     <div className="form-group">
                       <label htmlFor="name">First Name</label>
-                      <Field name="name" type="text" className="form-control input-text" aria-describedby="nameHelp" placeholder=' '
-                        required />
-                      <ErrorMessage name="name" component="div" className="form__error text-danger" />
+                      <Field
+                        name="name"
+                        type="text"
+                        className="form-control input-text"
+                        aria-describedby="nameHelp"
+                        placeholder=" "
+                        required
+                      />
+                      <ErrorMessage
+                        name="name"
+                        component="div"
+                        className="form__error text-danger"
+                      />
                     </div>
                     <div className="form-group">
                       <label htmlFor="surname">Last Name</label>
-                      <Field name="surname" type="email" className="form-control input-text" aria-describedby="surnameHelp" placeholder=' '
-                        required />
-                      <ErrorMessage name="surname" component="div" className="form__error text-danger" />
+                      <Field
+                        name="surname"
+                        type="email"
+                        className="form-control input-text"
+                        aria-describedby="surnameHelp"
+                        placeholder=" "
+                        required
+                      />
+                      <ErrorMessage
+                        name="surname"
+                        component="div"
+                        className="form__error text-danger"
+                      />
                     </div>
                     <div className="form-group">
                       <div className="d-flex flex-wrap justify-content-between align-items-center">
                         <label htmlFor="email">Email</label>
-                        <a href="#" className="tip">Forgot Email?</a>
+                        <a href="#" className="tip">
+                          Forgot Email?
+                        </a>
                       </div>
-                      <Field name="email" type="email" className="form-control input-text" aria-describedby="emailHelp" placeholder=' '
-                        required />
-                      <ErrorMessage name="email" component="div" className="form__error text-danger" />
+                      <Field
+                        name="email"
+                        type="email"
+                        className="form-control input-text"
+                        aria-describedby="emailHelp"
+                        placeholder=" "
+                        required
+                      />
+                      <ErrorMessage
+                        name="email"
+                        component="div"
+                        className="form__error text-danger"
+                      />
                     </div>
                     <div className="form-group">
                       <div className="d-flex flex-wrap justify-content-between align-items-center">
                         <label htmlFor="email">Password</label>
-                        <a href="#" className="tip">Forgot Password?</a>
+                        <a href="#" className="tip">
+                          Forgot Password?
+                        </a>
                       </div>
-                      <Field name="password" type="password" className="form-control input-text" aria-describedby="passwordHelp" placeholder=' '
-                        required />
-                      <ErrorMessage name="password" component="div" className="form__error text-danger" />
+                      <Field
+                        name="password"
+                        type="password"
+                        className="form-control input-text"
+                        aria-describedby="passwordHelp"
+                        placeholder=" "
+                        required
+                      />
+                      <ErrorMessage
+                        name="password"
+                        component="div"
+                        className="form__error text-danger"
+                      />
                     </div>
                     <div className="submit d-flex flex-wrap justify-content-between align-items-center">
-                      <Link to="/sign-in" className="tip">Already have an account?</Link>
-                      <button type="submit" className='btn btn-lg blue waves-effect waves-light submit-btn' disabled={isSubmitting}>Sign up</button>
+                      <Link to="/sign-in" className="tip">
+                        Already have an account?
+                      </Link>
+                      <button
+                        type="submit"
+                        className="btn btn-lg blue waves-effect waves-light submit-btn"
+                        disabled={isSubmitting}
+                      >
+                        Sign up
+                      </button>
                     </div>
-
                   </Form>
                 )}
               </Formik>
-
             </div>
           </div>
         </div>
-      </div >
-    </div >
+      </div>
+    </div>
   );
 };
 
--- src/pages/course/course.tsx
+++ src/pages/course/course.tsx
@@ -1,28 +1,27 @@
 import React, { FC, useEffect, useState } from 'react';
 import { Route, Redirect, useParams } from 'react-router-dom';
 import { observer, inject } from 'mobx-react';
-import './course.scss';
 import Store from '../../store';
-
-import { Teaching } from '../../components/Illustrations';
 import CreateAnnouncement from '../../components/CreataAnnouncement';
-type IProps = {
-  store?: Store
-};
 
+import './course.scss';
+
+type IProps = {
+  store?: Store;
+};
 
 const Course: FC<IProps> = ({ store }) => {
   const [unMount, setUnMount] = useState(false);
-  let { id } = useParams();
-  const CourseID = id;
+  const { id: CourseID } = useParams();
 
   useEffect(() => {
     const fetchData = async () => {
-      await Promise.all([store!.course.fetchCourse(CourseID), store!.announcement.fetchAllAnnouncements(CourseID)]);
+      await Promise.all([
+        store!.course.fetchCourse(CourseID),
+        store!.announcement.fetchAllAnnouncements(CourseID),
+      ]);
 
       setUnMount(true);
-      console.log(store);
-
 
       // unsubscribe
       return () => {
@@ -33,63 +32,40 @@ const Course: FC<IProps> = ({ store }) => {
     fetchData();
   }, []);
 
+  const {
+    course: { course },
+    user: { user, isTeacher },
+  } = store!;
 
-
-
-  const { course: { course }, user: { user, isTeacher } } = store!;
-  let content;
-  /*if (!courses || courses.length === 0) {
-    content = (
-      <>
-        <Teaching className="teaching-illustrator" />
-        <p className="teaching-p">You have no class right now.</p>
-      </>
-    );
-  } else {
-    content = (
-      <div className="Home-classes">
-        {courses.map(function (item, i) {
-          return <ClassCard key={i} card={item} />;
-        })}
-      </div>
-    );
-  }*/
-  console.log(user);
-  return (
-    unMount ?
-      course ? (
-        <div className="course-container">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-4">
-                <div className="border">Left Side Component</div>
+  return unMount ? (
+    course ? (
+      <div className="course-container">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4">
+              <div className="border">Left Side Component</div>
+            </div>
+            <div className="col-sm-7 pl-sm-3 pr-sm-3 border course_container__content">
+              <div
+                className={!isTeacher ? 'd-none' : 'course-container__content__create-announcement'}
+              >
+                <CreateAnnouncement />
               </div>
-              <div className="col-sm-7 pl-sm-3 pr-sm-3 border course_container__content">
-                <div className={`${!isTeacher ? 'd-none' : 'course-container__content__create-announcement'}`}>
-                  <CreateAnnouncement />
-                </div>
-                <div className={`${!isTeacher ? 'd-none' : 'course-container__content__timeline'}`}>
-                  <h2>Timeline</h2>
-                </div>
+              <div className={!isTeacher ? 'd-none' : 'course-container__content__timeline'}>
+                <h2>Timeline</h2>
               </div>
             </div>
           </div>
-
         </div>
-      )
-        :
-        <Redirect
-          to={{
-            pathname: '/',
-          }}
-        />
-      :
-      null
-  );
+      </div>
+    ) : (
+      <Redirect
+        to={{
+          pathname: '/',
+        }}
+      />
+    )
+  ) : null;
 };
-
-//const courses = [];
-
-
 
 export default inject('store')(observer(Course));
