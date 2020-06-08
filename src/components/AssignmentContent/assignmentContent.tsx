0761147dabd763c30c85a87322d7d60f2c9fbcba
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-06-08 18:57:11 +0300

    due date of assignment converted to string

--- src/components/AssignmentContent/assignmentContent.tsx
+++ src/components/AssignmentContent/assignmentContent.tsx
@@ -31,12 +31,29 @@ const AssignmentContent: FC<IProps> = ({
   let hoursString = hours < 10 ? `0${hours}` : hours;
   const dateString = `${dateTime.toLocaleDateString()} ${hoursString}:${minutesString}`;
 
+  const splitDueDate = dueDate.toString().split(' ');
+  const splitDate = splitDueDate[0].split('.');
+  const splitTime = splitDueDate[1].split(':');
+  const due = new Date(
+    Number(splitDate[2]),
+    Number(splitDate[1]) - 1,
+    Number(splitDate[0]),
+    Number(splitTime[0]),
+    Number(splitTime[1]),
+    0,
+  );
+  const dMinutes = due.getMinutes();
+  const dHours = due.getHours();
+
+  let dMinutesString = dMinutes < 10 ? `0${dMinutes}` : dMinutes;
+  let dHoursString = dHours < 10 ? `0${dHours}` : dHours;
+  const dDateString = `${due.toLocaleDateString()} ${dHoursString}:${dMinutesString}`;
+
   return (
     <div className="assignment-detail">
       <div className="assignment-firstline">
         <span>{teacherName}</span>
-        {/* TODO: handle duedate time string */}
-        <span className="due-date">Due Date: ${dueDate.toLocaleDateString()}</span>
+        <span className="due-date">Due Date: {dDateString}</span>
       </div>
       <div className="assignment-secondline">
         <span>{dateString}</span>
