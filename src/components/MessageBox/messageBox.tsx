e2d2de29bea2edd58846fb17a81bf837009d175b
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-06-20 23:41:06 +0300

    check new messages periodically & scroll to bottom

--- src/components/MessageBox/messageBox.tsx
+++ src/components/MessageBox/messageBox.tsx
@@ -1,8 +1,16 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect, useRef } from 'react';
 import './messageBox.scss';
 import Store from '../../store';
 import IUser from '../../interfaces/user';
-import { Box, IconButton, Button, Typography, FormControl, Input, InputAdornment } from '@material-ui/core';
+import {
+  Box,
+  IconButton,
+  Button,
+  Typography,
+  FormControl,
+  Input,
+  InputAdornment,
+} from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import { Delete as DeleteIcon, Edit as EditIcon } from '@material-ui/icons';
 import send from '../../static/icons/send.svg';
@@ -10,18 +18,45 @@ import { inject, observer } from 'mobx-react';
 
 interface IProps {
   store?: Store;
-  receiverId: number
+  receiverId: number;
 }
-const MessageBox: FC<IProps> = ({ receiverId, store: { message: { messages, fetchAllMessages, sendMessage }, user: { user },
-  course: { studentsOfCourse } } }) => {
-  const receiver = studentsOfCourse.filter(s => s.id === receiverId)[0];
+const MessageBox: FC<IProps> = ({
+  receiverId,
+  store: {
+    message: { messages, fetchAllMessages, sendMessage },
+    user: { user, getById },
+  },
+}) => {
+  const [receiver, setReceiver] = useState({ name: '', surname: '' });
   const [messageInput, setMessageInput] = useState('');
+
+  const messageBoxRef = useRef();
+
   useEffect(() => {
     const fetchMessages = async () => {
-      await fetchAllMessages(receiverId);
+      const newMessage = await fetchAllMessages(receiverId);
+      if (newMessage) {
+        scrollToBottom();
+      }
     };
-    fetchMessages();
+    const fetchAll = async () => {
+      const res = await getById(receiverId);
+      setReceiver(res);
+      fetchMessages();
+    };
+    fetchAll();
+
+    const intervalID = setInterval(fetchMessages, 2000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
   }, []);
+
+  const scrollToBottom = () => {
+    const node = messageBoxRef.current as any;
+    node.scrollTop = node.scrollHeight;
+  };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setMessageInput(event.target.value);
@@ -30,40 +65,39 @@ const MessageBox: FC<IProps> = ({ receiverId, store: { message: { messages, fetc
   const handleMessageSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     if (messageInput) {
-      await sendMessage(receiverId, messageInput);
+      const newMessage = await sendMessage(receiverId, messageInput);
       setMessageInput('');
+      setTimeout(() => {
+        if (newMessage) {
+          scrollToBottom();
+        }
+      }, 200);
     }
   };
-  return (
+  return receiver ? (
     <div className="message-box">
       <div className="message-box__header">
         <div className="first-letter">{receiver.name[0] + receiver.surname[0]}</div>
-        <div className="receiver-name">{receiver.name}{' '}{receiver.surname} </div>
+        <div className="receiver-name">
+          {receiver.name} {receiver.surname}{' '}
+        </div>
       </div>
-      <div className="message-box__body">
-        {
-          messages.map((m, index) => {
-            if (m.receiver.id === user.id) {
-              return (
-                <div key={index} className="incoming">
-                  <div className="bubble">
-                    {m.content}
-                  </div>
-                </div>
-              );
-            }
-            else {
-              return (
-                <div key={index} className="outgoing">
-                  <div className="bubble">
-                    {m.content}
-                  </div>
-                </div>
-              );
-            }
-          })
-        }
-
+      <div className="message-box__body" ref={messageBoxRef}>
+        {messages.map((m, index) => {
+          if (m.receiver.id === user.id) {
+            return (
+              <div key={index} className="incoming">
+                <div className="bubble">{m.content}</div>
+              </div>
+            );
+          } else {
+            return (
+              <div key={index} className="outgoing">
+                <div className="bubble">{m.content}</div>
+              </div>
+            );
+          }
+        })}
       </div>
       <div className="message-box__footer">
         <form noValidate onSubmit={handleMessageSubmit}>
@@ -79,21 +113,20 @@ const MessageBox: FC<IProps> = ({ receiverId, store: { message: { messages, fetc
                 classes: {
                   root: 'cssOutlinedInput',
                   focused: 'cssFocused',
-                  notchedOutline: 'notchedOutline'
-                }
+                  notchedOutline: 'notchedOutline',
+                },
               }}
               value={messageInput}
               onChange={handleInputChange}
             />
-            <Button type="submit" className="submit-btn" disabled={!messageInput ? true : false} >
+            <Button type="submit" className="submit-btn" disabled={!messageInput ? true : false}>
               <img src={send} alt="submit" />
             </Button>
           </FormControl>
         </form>
       </div>
     </div>
-  );
+  ) : null;
 };
 
 export default inject('store')(observer(MessageBox));
-
--- src/store/message.ts
+++ src/store/message.ts
@@ -2,33 +2,55 @@ import Store from '.';
 import { observable } from 'mobx';
 import IMessage from '../interfaces/message';
 
-
 export default class Message {
   private url = {
     base: '/messages',
   };
   @observable
-  public messages: Array<IMessage> = [];  // this holds the messages between two.
+  public messages: Array<IMessage> = []; // this holds the messages between two.
 
-  constructor(private store: Store) { }
+  constructor(private store: Store) {}
 
-  public fetchAllMessages = async (receiverId: number): Promise<void> => {
+  public fetchAllMessages = async (receiverId: number): Promise<boolean> => {
+    const prevLength = this.messages.length;
+
     const url = `${this.url.base}/${receiverId}`;
     const response = await this.store.api.fetch({ url }, 200);
     const { status } = response;
     if (!status) {
       this.messages = response;
-    }
-    else {
+    } else {
       this.messages = [];
     }
+
+    if (this.messages.length != prevLength) {
+      return true;
+    }
+    return false;
   };
-  public sendMessage = async (receiverId: number, content: string): Promise<void> => {
+
+  public sendMessage = async (receiverId: number, content: string): Promise<boolean> => {
     const url = `${this.url.base}/${receiverId}`;
     const form = { content };
     const response = await this.store.api.fetch({ url, method: 'post', form }, 200);
-    if (!response.status) { // which means if only data is sent from api.ts.
-      this.fetchAllMessages(receiverId);
+    if (!response.status) {
+      // which means if only data is sent from api.ts.
+      const newMessage = this.fetchAllMessages(receiverId);
+      if (newMessage) {
+        return true;
+      }
+      return false;
     }
-  }
+  };
+
+  public getAllChatters = async (): Promise<any> => {
+    const url = `${this.url.base}/chatters`;
+    const response = await this.store.api.fetch({ url }, 200);
+    const { status } = response;
+    if (!status) {
+      return response;
+    } else {
+      return null;
+    }
+  };
 }
